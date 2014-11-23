5f7148eaf5bcbfd44afa52e97d4ffab4f674d893
lhsantosazs <lhsantosazs@gmail.com>
2014-11-23 15:34:38 -0200

    curriculos relacionados comentado por enquanto

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 $(document).ready(function(){
 	$.get( "http://rarolabs.com.br:88/alunos.json", function(data) {
 		console.log(data);
-		data.forEach(function(value, index) {
-			var curriculo = '<a href="'+ value.link_github +'"> <h4>' + value.nome + '</h4> + <p>' + value.mini_curriculo + '</p></a>'
-			$('#curriculos_relacionados').append(curriculo);
-		});
+		// data.forEach(function(value, index) {
+		// 	var curriculo = '<a href="'+ value.link_github +'"> <h4>' + value.nome + '</h4> <p>' + value.mini_curriculo + '</p></a>'
+		// 	$('#curriculos_relacionados').append(curriculo);
+		// });
 	});
 })
