f04e85f496981109b2165bae03e8c404f5bf94f7
lhsantosazs <lhsantosazs@gmail.com>
2014-11-23 15:20:46 -0200

    foreach

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Todo o java script do jogo deve ser colocado aqui
 $(document).ready(function(){
-	$.get( "http://rarolabs.com.br:88/alunos.json", function( data ) {
-		data.each(function(index, value) {
+	$.get( "http://rarolabs.com.br:88/alunos.json", function(data) {
+		data.forEach(function(index, value) {
 			console.log( index + ": " + value );
 		});
 	});
