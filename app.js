9647c98fba91be9409b4cdcdc5dadcbbbd9dbf90
lhsantosazs <lhsantosazs@gmail.com>
2014-11-23 15:17:32 -0200

    each dentro da api

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //Todo o java script do jogo deve ser colocado aqui
 $(document).ready(function(){
 	$.get( "http://rarolabs.com.br:88/alunos.json", function( data ) {
-	  console.log(data);
-	  alert(data);
+		data.each(function(index, value) {
+			console.log( index + ": " + value );
+		});
 	});
 })
