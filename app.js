dd56bf4e783a42662d5616d84d53952a9ed388c5
lhsantosazs <lhsantosazs@gmail.com>
2014-11-23 15:23:54 -0200

    append

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //Todo o java script do jogo deve ser colocado aqui
 $(document).ready(function(){
 	$.get( "http://rarolabs.com.br:88/alunos.json", function(data) {
-		data.forEach(function(index, value) {
-			console.log( index + ": " + value );
+		console.log(data);
+		data.forEach(function(value, index) {
+			$('#curriculos_relacionado').append(value);
 		});
 	});
 })
