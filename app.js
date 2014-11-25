947cec5d92617b4b48b431dbf47c8bad636894b4
lhsantosazs <lhsantosazs@gmail.com>
2014-11-25 01:48:13 -0200

    Finalização do curriculo

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 //Todo o java script do jogo deve ser colocado aqui
 $(document).ready(function(){
+	//Pega os dados da api e insere no html
 	$.get( "http://rarolabs.com.br:88/alunos.json", function(data) {
 		console.log(data);
-		// data.forEach(function(value, index) {
-		// 	var curriculo = '<a href="'+ value.link_github +'"> <h4>' + value.nome + '</h4> <p>' + value.mini_curriculo + '</p></a>'
-		// 	$('#curriculos_relacionados').append(curriculo);
-		// });
+		data.forEach(function(value, index) {
+			var curriculo = '<a class="list-group-item" href="'+ value.link_html + '">'+ 
+							'<h3 class="list-group-item-heading">' + value.nome + '</h3>' +
+							'<p class="list-group-item-text">' + value.mini_curriculo + '</p>'+
+							'</a>'
+
+			$('#curriculos_relacionados .panel-body .list-group').append(curriculo);
+		});
+	});
+
+	//Ao clica no titulo da section o corpo é escondido/exibido
+	$('.panel-heading').click(function(){
+		$(this).next().toggle();
 	});
 })
